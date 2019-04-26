f04be5bb816e46469a308d77774a6e43700bc322
zhuimenran <2161434564@qq.com>
2019-04-26 12:58:44 +0800

    add Student.java 泛型的用例

---
+++ src/com/txb/array/test/Student.java
@@ -0,0 +1,31 @@
+package com.txb.array.test;
+
+import com.txb.array.util.Array;
+
+public class Student {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "student [name=" + name + ", score=" + score + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
