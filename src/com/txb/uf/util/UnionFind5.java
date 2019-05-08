34b658dc416537d00590b8a87a3e55d1e95e13f6
zhuimenran <2161434564@qq.com>
2019-05-08 15:34:37 +0800

    add  路径优化

---
+++ src/com/txb/uf/util/UnionFind5.java
@@ -0,0 +1,78 @@
+package com.txb.uf.util;
+
+
+/**
+ * 并查集 UnionFind4  的优化，进行路径压缩
+ * 把路径深度降低，比如从一个链表变成一个树，几叉树都行
+ * 可以在find()的时候进行，每次查询的时候，把该节点指向父节点的父节点，这样就压缩了
+ */
+public class UnionFind5 implements UF {
+
+    //parent[i]表示第i个元素指向的节点的位置
+    private int[] parent;
+
+    private int[] rank;//rank[i]表示以i为根的集合中所表示树的层数
+
+    public UnionFind5(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        //初始化的时候，每一个节点都指向自己，那么都是一个独立的树，并没有连接的关系
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 1;//
+        }
+    }
+
+    //判断两个元素是否所属一个集合，
+    //如果这两个元素相连，那么他们肯定执行同一个父节点
+    //父节点判断的依据就是元素p的值和坐标相等
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    //时间复杂度是O(h)  h 是树的高度
+    //查找元素p所对应的集合编号
+    private int find(int p) {
+        //直到元素p 指向自己，说明这个就是p的根节点。
+        while (p != parent[p]) {
+            //把节点的父节点指向父节点的父节点。
+            //4->2->6->3->5->9
+            //4的parent[p]=2
+            // 2的parent是6
+            //parent[4]=parent[2]=6
+            // 这样下来，就变成了 4 和2都指向 6->3->5->9
+            parent[p] = parent[parent[p]];
+            //再次循环
+            p = parent[p];
+        }
+        return p;
+    }
+
+    //合并元素p和q所属的集合
+    @Override
+    public void unionElements(int p, int q) {
+        //合并的依据就是，找到这两个元素的根节点，然后把p的根节点下标改为q的根节点
+        int rootP = find(p);//p对应的根节点的位置
+
+        int rootQ = find(q);//q对应根节点的位置
+        if (rootP == rootQ) {
+            return;//值相等，是连接的，也就是已经并过了
+        }
+        //不相等，根据两个元素所对应的rank不同判断合并方向，rank小的合并到rank多的上去
+        if (rank[rootP] < rank[rootQ]) {//说明q根节点中集合个数多，那么让p的根节点指向q的根节点
+            parent[rootP] = parent[rootQ];
+            //这个时候rank[rooQ]是不需要变化的，因为一个层级底的挂载到一个层级高的节点上去，层级是不会改变的
+        } else if (rank[rootP] > rank[rootQ]) {
+            parent[rootQ] = parent[rootP];
+        } else {//层级两个相等，那么就需要有一个层级挂载到另一个层级上，那么这个层级肯定会加一的
+            parent[rootP] = parent[rootQ];//p的值变成了q,那么q的层级得加一
+            rank[rootQ] += 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+}
